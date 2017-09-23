15d3d54c9c2f3544f19ecaec2b987008e6b058d6
DATBOII\iMrBa_000 <TychoZaal@gmail.com>
2017-09-23 14:45:44 +0200

    Player movement, Target movement, basic scene

---
+++ Prototype 2 PGD 2/Assets/Scripts/MousePos.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MousePos : MonoBehaviour
+{
+
+    public float speed;
+
+    float moveHorizontal = 0;
+    float moveVertical = -20;
+
+    void Start()
+    {
+        transform.position = new Vector3(-20, 0f, 0f);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            moveHorizontal++;
+        }
+
+          if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            moveHorizontal--;
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            moveVertical--;
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            moveVertical++;
+        }
+
+        transform.position = new Vector3(moveVertical, 0f, moveHorizontal);
+    }
+}
---
+++ Prototype 2 PGD 2/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody rb;
+    float moveHorizontal = 0;
+
+    void Start()
+    {
+        transform.position = new Vector3(0, 10, 0);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            moveHorizontal++;
+            if (moveHorizontal > 4)
+            {
+                moveHorizontal = 4;
+            }
+        }
+
+        /* if (moveHorizontal < 5 || moveHorizontal > -5)
+        {
+            moveHorizontal = 0;
+        } */
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            moveHorizontal--;
+            if (moveHorizontal < -4)
+            {
+                moveHorizontal = -4;
+            }
+        }
+
+        transform.position = new Vector3(0.0f, 10f, (float)moveHorizontal);
+    }
+}
